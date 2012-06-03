7b746f08bd92ce50709d8a1a53fab3f3ebb170fc
carlcarl <carlcarlking@gmail.com>
2012-06-03 11:45:50 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# README #
+
+This is a sample program to generate URL with CloudStack API.
+
---
+++ generate-url.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# by carlcarl
+
+import getopt, sys
+import hmac
+import hashlib
+import base64
+import urllib
+
+def formattedCmd(api, cmd):
+    s = 'apiKey=' + api + '&' + cmd
+    return s
+
+def encypt(string, key):
+    h = hmac.new(key, string, hashlib.sha1)
+    return base64.b64encode(h.digest())
+
+def formattedUrl(baseUrl, api, cmd, signature):
+    url = baseUrl + '?' + formattedCmd(api, cmd) + '&' + urllib.urlencode({'signature': signature})# signature=' + urllib.quote(signature)
+    return url
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'u:a:s:', ['help', 'output='])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
+
+    baseUrl = 'http://*.*.*.*:8080/client/api'
+    cmd = api = secret = None
+
+    for o, a in opts:
+        if o == '-u':
+            cmd = a
+        elif o == '-a':
+            api = a
+        elif o == '-s':
+            secret = a
+        else:
+            assert False, 'unhandled option'
+
+    newCmd = formattedCmd(api, cmd).lower()
+    signature = encypt(newCmd, secret)
+    url = formattedUrl(baseUrl, api, cmd, signature)
+
+    print '\n' + url
+
+if __name__ == '__main__':
+    main()
+
